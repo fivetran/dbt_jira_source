ba69a1864395ed4b574821ebfb1bbe61df364c2a
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-11-23 20:04:28 -0500

    stg status

--- models/stg_jira__status.sql
+++ models/stg_jira__status.sql
@@ -1,7 +1,20 @@
-\n\n
+with base as (
+
+    select * 
+    from {{ ref('stg_jira__status_tmp') }}
+),
+
+fields as (
+
+    select
         {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_jira__status_tmp')),
                 staging_columns=get_status_columns()
             )
         }}
+
+    from base
+)
+
+select * from fields
