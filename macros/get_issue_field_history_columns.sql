bb1049f4bd5a29b491565513cee2114f84af87d0
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-11-25 16:38:49 -0500

    fixed redshift reserved words issue

--- macros/get_issue_field_history_columns.sql
+++ macros/get_issue_field_history_columns.sql
@@ -4,9 +4,15 @@
     {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
     {"name": "field_id", "datatype": dbt_utils.type_string()},
     {"name": "issue_id", "datatype": dbt_utils.type_int()},
-    {"name": "time", "datatype": dbt_utils.type_timestamp()},
     {"name": "value", "datatype": dbt_utils.type_string()}
 ] %}
+
+{% if target.type == 'redshift' %}
+ {{ columns.append( {"name": "time", "datatype": dbt_utils.type_timestamp(), "quote": True } ) }}
+{% else %}
+ {{ columns.append( {"name": "time", "datatype": dbt_utils.type_timestamp()} ) }}
+{% endif %}
+
 
 {{ return(columns) }}
 
--- models/stg_jira__issue_field_history.sql
+++ models/stg_jira__issue_field_history.sql
@@ -25,7 +25,7 @@ final as (
         field_id,
         issue_id,
         "time" as updated_at,
-        value as field_value,
+        "value" as field_value,
         _fivetran_synced
 
     from fields
