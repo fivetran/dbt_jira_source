3be19f641ec749ff121e8bde2d1e14ecec930509
fivetran-kristin <kristin@fivetran.com>
2021-03-19 14:28:30 -0700

    add disable components logic

--- models/stg_jira__component.sql
+++ models/stg_jira__component.sql
@@ -1,3 +1,4 @@
+{{ config(enabled=var('jira_using_components', True)) }}
 
 with base as (
 
--- models/tmp/stg_jira__component_tmp.sql
+++ models/tmp/stg_jira__component_tmp.sql
@@ -1,1 +1,3 @@
+{{ config(enabled=var('jira_using_components', True)) }}
+
 select * from {{ var('component') }}
