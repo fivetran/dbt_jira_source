1b64575215233dffebb153fc898ca5cfddbeff03
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-04-15 17:17:36 -0500

    review updates

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -18,12 +18,12 @@ cd integration_tests
 dbt deps
 
 if [ "$db" = "databricks-sql" ]; then
-dbt seed --vars '{jira_schema: sqlw_tests}' --target "$db" --full-refresh
-dbt compile --vars '{jira_schema: sqlw_tests}' --target "$db"
-dbt run --vars '{jira_schema: sqlw_tests}' --target "$db" --full-refresh
-dbt test --vars '{jira_schema: sqlw_tests}' --target "$db"
-dbt run --vars '{jira_schema: sqlw_tests, jira_using_priorities: false, jira_using_sprints: false, jira_using_components: false, jira_using_versions: false, jira_field_grain: 'field_name'}' --target "$db" --full-refresh
-dbt test --target "$db"
+dbt seed --vars '{jira_schema: jira_source_integrations_tests_sqlw}' --target "$db" --full-refresh
+dbt compile --vars '{jira_schema: jira_source_integrations_tests_sqlw}' --target "$db"
+dbt run --vars '{jira_schema: jira_source_integrations_tests_sqlw}' --target "$db" --full-refresh
+dbt test --vars '{jira_schema: jira_source_integrations_tests_sqlw}' --target "$db"
+dbt run --vars '{jira_schema: jira_source_integrations_tests_sqlw, jira_using_priorities: false, jira_using_sprints: false, jira_using_components: false, jira_using_versions: false, jira_field_grain: 'field_name'}' --target "$db" --full-refresh
+dbt test --vars '{jira_schema: jira_source_integrations_tests_sqlw, jira_using_priorities: false, jira_using_sprints: false, jira_using_components: false, jira_using_versions: false, jira_field_grain: 'field_name'}' --target "$db"
 
 else
 dbt seed --target "$db" --full-refresh
@@ -31,7 +31,7 @@ dbt compile --target "$db"
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 dbt run --vars '{jira_using_priorities: false, jira_using_sprints: false, jira_using_components: false, jira_using_versions: false, jira_field_grain: 'field_name'}' --target "$db" --full-refresh
-dbt test --target "$db"
+dbt test --vars '{jira_using_priorities: false, jira_using_sprints: false, jira_using_components: false, jira_using_versions: false, jira_field_grain: 'field_name'}' --target "$db"
 fi
 
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
